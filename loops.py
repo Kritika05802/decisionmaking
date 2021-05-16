efdb5a44e015d65e9b8cb98aa7fd877825d0c2f1
Kritika Saxena <82437949+Kritika05802@users.noreply.github.com>
2021-05-16 20:53:30 +0530

    Range of positive number

---
+++ loops.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+l1= [12, -7, 5, 64, -14, 3, -4]
+for num in l1:
+    if num>0:
+        print(num)
+
+
+# In[2]:
+
+
+l2= [12, 14, -95, 3, 24, -5]
+for num in l2:
+    if num>0:
+        print(num)
+
+
+# In[ ]:
+
+
+
+
